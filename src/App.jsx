ac972fc842dbc69bd7494b2ee511f56a6b1c118f
Anmol Jadgilwar <developer.anmol108@gmail.com>
2025-02-21 20:12:15 +0530

    initial comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import AuthService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
 import "./App.css";
 
 const App = () => {
   // console.log(import.meta.env.VITE_TECHBLOG_APPWRITE);
+  const [loading, setLoading] = useState(true);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(
+      AuthService.getCurrentUser()
+        .then((userData) => {
+          if (userData) {
+            dispatch(login({ userData }));
+          } else {
+            dispatch(logout());
+          }
+        })
+        .finally()
+    );
+  }, [dispatch]);
 
   return (
     <>
       <h1>A blog App in Appwrite</h1>
     </>
+  );
+  return !loading ? (
+    <div className="App bg-orange-400">
+      <h1>A blog App in Appwrite</h1>
+    </div>
+  ) : (
+    <div>Loading</div>
   );
 };
 
--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -12,5 +12,11 @@ const authSlice = createSlice({
       state.state = true;
       state.userData = action.payload.userData;
     },
+    logout: (state) => {
+      state.state = false;
+      state.userData = null;
+    },
   },
 });
+export const { login, logout } = authSlice.actions;
+export default authSlice.reducer;

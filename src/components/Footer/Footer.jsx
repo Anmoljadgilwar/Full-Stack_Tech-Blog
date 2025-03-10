c863c5adee307e5b0dd0a552d3c30a484d2eb8ee
Anmol Jadgilwar <developer.anmol108@gmail.com>
2025-03-10 20:28:14 +0530

    initial commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -35,7 +35,7 @@ function Footer() {
                 </li>
                 <li className="mb-4">
                   <Link
-                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                    className=" text-base font-medium text-gray-900 hover:text-gray-600"
                     to="/"
                   >
                     Pricing
